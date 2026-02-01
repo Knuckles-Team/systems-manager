f22f5740caeac564abbd0ad48b4c8838fb2f0bf7
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-01 02:24:45 -0600

    Bump version: 1.1.25 → 1.1.26

--- systems_manager/systems_manager.py
+++ systems_manager/systems_manager.py
@@ -16,7 +16,7 @@ import psutil
 from typing import List, Dict, Union
 from abc import ABC, abstractmethod
 
-__version__ = "1.1.25"
+__version__ = "1.1.26"
 
 
 def setup_logging(
--- systems_manager/systems_manager_mcp.py
+++ systems_manager/systems_manager_mcp.py
@@ -28,7 +28,7 @@ from systems_manager.systems_manager import (
     WindowsManager,
 )
 
-__version__ = "1.1.25"
+__version__ = "1.1.26"
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
