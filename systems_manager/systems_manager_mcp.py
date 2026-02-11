336d33fff301a57bf457d1a65ad9f4c2af5532eb
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-10 23:08:26 -0600

    Bump version: 1.2.3 → 1.2.4

--- systems_manager/systems_manager_mcp.py
+++ systems_manager/systems_manager_mcp.py
@@ -28,7 +28,7 @@ from systems_manager.systems_manager import (
     WindowsManager,
 )
 
-__version__ = "1.2.3"
+__version__ = "1.2.4"
 
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
