a492eaebb7f185f0d2707da49cb1fb8381a19be9
KnucklesSG1 <knucklessg1@gmail.com>
2026-03-31 01:11:12 -0500

    Bump version: 1.2.52 → 1.2.53

--- systems_manager/agent_server.py
+++ systems_manager/agent_server.py
@@ -13,7 +13,7 @@ from agent_utilities import (
     load_identity,
 )
 
-__version__ = "1.2.52"
+__version__ = "1.2.53"
 
 logging.basicConfig(
     level=logging.INFO,
--- systems_manager/systems_manager.py
+++ systems_manager/systems_manager.py
@@ -20,7 +20,7 @@ import psutil
 from typing import List, Dict, Union
 from abc import ABC, abstractmethod
 
-__version__ = "1.2.52"
+__version__ = "1.2.53"
 
 
 def setup_logging(
